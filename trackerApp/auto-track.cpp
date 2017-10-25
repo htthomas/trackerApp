9d37b5784b801f6c60973749a5ef87e26cdc4be1
Henry Thomas <henry.bg2md@gmail.com>
2017-10-25 01:29:46 -0800

    moved a few things around

--- trackerApp/auto-track.cpp
+++ trackerApp/auto-track.cpp
@@ -20,11 +20,11 @@
 using namespace std;
 using namespace cv;
 
-Point detectAndDisplay(Mat frame);
-String trackIt(Point meta, double frameWidth, double frameHeight, double size);
+Rect detectAndDisplay(Mat frame);
+String trackIt(Rect face, double frameWidth, double frameHeight, double size);
 
 Mat frame; //frame to detect faces on
-CascadeClassifier cascade1;
+CascadeClassifier cascade;
 string cascade_name = "headshoulders.xml"; //pick cascade here
 
 int main(int argc, char * argv[])
@@ -90,7 +90,7 @@ int main(int argc, char * argv[])
 	{
 		printf("Error loading video capture"); return 1;
 	}
-	if (!cascade1.load(cascade_name))
+	if (!cascade.load(cascade_name))
 	{
 		printf("Error loading cascade"); return 1;
 	}
@@ -108,6 +108,7 @@ int main(int argc, char * argv[])
 
 	int j = 0;
 	string str;
+	Rect face;
 	Point center;
 	string prv_cmd = "camera pan stop";
 	POINT prv_center;
@@ -121,14 +122,17 @@ int main(int argc, char * argv[])
 
 		if (!frame.empty())
 		{
-			center = detectAndDisplay(frame);
+			face = detectAndDisplay(frame);
+			center.x = face.x + face.width / 2;
+			center.y = face.y + face.height / 2;
+
 			if (center.x == 0 && center.y == 0) {
 				center.x = prv_center.x;
 				center.y = prv_center.y;
 			}
-			if (j % 12 == 0)
+			if (j % 5 == 0)
 			{
-				str = trackIt(center, frameWidth, frameHeight, size);
+				str = trackIt(face, frameWidth, frameHeight, size);
 				if (str != prv_cmd)
 				{
 					telnet_client.write(str);
@@ -149,26 +153,27 @@ int main(int argc, char * argv[])
 		if (27 == char(waitKey(10))) break; //wait for ESC key to exit
 	}
 
-	#ifdef POSIX
-		tcsetattr(0, TCSANOW, &stored_settings);
-	#endif
+#ifdef POSIX
+	tcsetattr(0, TCSANOW, &stored_settings);
+#endif
 
 	return 0;
 }
 
 //detect faces and return their location as a point
-Point detectAndDisplay(Mat frame)
+Rect detectAndDisplay(Mat frame)
 {
 	vector<Rect> faces; //list of faces in frame
 	Mat frame_gray; //grayscale frame
 	Point botLeft; //point of rectangle
 	Point topRight; //point of rectangle
 	Point center; //center of rect
+	Rect faceRect;
 	int size; //size of face
 
 	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
 	//params: 1-frame 2-vector 3-scale 4-minNeighbors 5-flags 6-minsize 7-maxsize
-	cascade1.detectMultiScale(frame_gray, faces, 1.2, 3, CV_HAAR_FIND_BIGGEST_OBJECT, Size(100, 100));
+	cascade.detectMultiScale(frame_gray, faces, 1.2, 3, CV_HAAR_FIND_BIGGEST_OBJECT, Size(100, 100));
 
 	//iterate through all detected faces and draw rectangles on frame
 	for (int i = 0; i < faces.size(); i++)
@@ -176,45 +181,91 @@ Point detectAndDisplay(Mat frame)
 		botLeft = Point(faces[i].x, faces[i].y);
 		topRight = Point((faces[i].x + faces[i].width), (faces[i].y + faces[i].height));
 		center = Point((faces[i].x + 0.5 * faces[i].width), (faces[i].y + 0.5 * faces[i].height));
+		faceRect = Rect(botLeft, topRight);
 		size = (faces[i].width * faces[i].height);
 		rectangle(frame, botLeft, topRight, Scalar(0, 255, 0), 2, 8, 0);
 	}
 	imshow("Automated Lecture Tracking", frame);
-	return center;
+	return faceRect;
 }
 
 //returns command to send to camera
-String trackIt(Point meta, double frameWidth, double frameHeight, double size)
+String trackIt(Rect face, double frameWidth, double frameHeight, double size)
 {
-	//double x = abs(48 * (meta.x / frameWidth - 0.5));
-	//int speed = (int)x;
+	Point meta = Point(face.x + face.width / 2, face.y + face.height / 2);
+	int x_weight = (int)abs(200 * (meta.x / frameWidth - 0.5));
+	int y_weight = (int)abs(200 * (meta.y / frameWidth - 0.5));
+	double lowerLimit = 0.4;
+	double upperLimit = 0.6;
 
-	if (meta.x == 0)
+	if ((meta.x > frameWidth*lowerLimit && meta.x < frameWidth*upperLimit) || meta.x == 0) {
+		x_weight = 0;
+	}
+	if ((meta.y > frameHeight*lowerLimit && meta.y < frameHeight*upperLimit) || meta.y == 0) {
+		y_weight = 0;
+	}
+
+	if (x_weight >= y_weight && x_weight > 0) {
+		if (meta.x - frameWidth / 2 > 0) {
+			return "camera pan right";
+		}
+		else {
+			return "camera pan left";
+		}
+	}
+	else if (x_weight < y_weight && y_weight > 0) {
+		if (meta.y - frameHeight / 2 > 0) {
+			return "camera tilt down";
+		}
+		else {
+			return "camera tilt up";
+		}
+	}
+	else if (x_weight == 0 && y_weight == 0) {
+		if (abs(meta.x - frameWidth / 2) > abs(meta.y - frameHeight / 2)) {
+			return "camera tilt stop";
+		}
+		else {
+			return "camera pan stop";
+		}
+	}
+
+	/*if (meta.x == 0)
 	{
-		return "camera pan stop";
+	return "camera pan stop";
 	}
 	if (meta.y == 0)
 	{
-		return "camera tilt stop";
+	return "camera tilt stop";
 	}
-	else if (meta.x < frameWidth * 0.35)
+	if (face.width > frameWidth / 3 && face.width < frameWidth / 2) {
+	return "camera zoom stop";
+	}
+	else if (meta.x < frameWidth * 0.3)
 	{
-		return "camera pan left";
+	//cout << "camera pan left";
+	return "camera pan left";
 	}
-	else if (meta.x > frameWidth * 0.65)
+	else if (meta.x > frameWidth * 0.7)
 	{
-		return "camera pan right";
+	//cout << "camera pan right";
+	return "camera pan right";
 	}
-	else if (meta.y < frameHeight * 0.35)
+	else if (meta.y < frameHeight *0.3)
 	{
-		return "camera tilt up";
+	return "camera tilt up";
 	}
-	else if (meta.y > frameHeight * 0.65)
+	else if (meta.y > frameHeight *0.7)
 	{
-		return "camera tilt down";
+	return "camera tilt down";
 	}
-	else
-	{
-		return "camera pan stop";
+	else if (face.width < frameWidth / 3) {
+	return "camera zoom in 2";
 	}
+	else if (face.width > frameWidth / 2) {
+	return "camera zoom out 2";
+	}
+	else {
+	return "camera zoom stop";
+	}*/
 }

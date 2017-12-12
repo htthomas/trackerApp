94c4003d53e9a64138f0e884afa9e58acb678b77
TevinGladden <31493107+TevinGladden@users.noreply.github.com>
2017-12-11 23:37:44 -0900

    Add files via upload
    More GUI functionality
    -static GUI window size
    -button text for accurate description of action performed
    -mode label for user to visually recognize the mode.

--- Lecture Tracker with GUI.py
+++ Lecture Tracker with GUI.py
@@ -20,11 +20,6 @@ import thread
 
 from Tkinter import *
 
-class TrackingApp:
-    def __init__(self):
-        pass
-    def displayStart(self, master):
-        print ("Start Flag", master.start_flag)
         
 class TrackingWindow:
     def __init__(self, start_flag, exit_flag):
@@ -34,56 +29,75 @@ class TrackingWindow:
         
         #Window Creation
         self.root = Tk()
-        self.root.title("Lecturer-Tracking App")
-        self.root.geometry("300x300")
-        self.can = Canvas(self.root, bg = 'red', height = 150, width = 150)
+        self.root.title("Lecture-Tracking App")
+        self.root.geometry("350x120")
+        self.root.resizable(width=False, height=False)
+        self.can = Canvas(self.root, height = 20, width = 50)
         self.can.place(relx = 0.5, rely = 0.5, anchor = CENTER)
         
-        #button creation
-        #start = Button(self.can, text="Automatic", width=50, command=threading.Thread(target=self.start).start())        
+        global label
+        label = Label(self.can, text = "Ready to Begin Tracking", bg = "white", height = 2, width = 50)
+        label.pack(side = TOP)
         
-        start = Button(self.can, text="Begin Tracking", width=50, command=lambda: self.begin())
+        #button creation
+        global start
+        start = Button(self.can, text="Begin Tracking", height = 2, width=50, command=lambda: self.begin())
         start.pack()
         
-        resume = Button(self.can, text="Resume Tracking", width=50, command=lambda: self.start())
+        global resume
+        resume = Button(self.can, text="", height = 2, width=50)
         resume.pack()
         
-        pause = Button(self.can, text="Pause Tracking", width=50, command=lambda: self.stop())
-        pause.pack()
-        
-        #frame = root
     def getStartFlag(self):
         return self.start_flag
     
-    def getStopFlag(self):
-        return self.stop_flag
+    def getExitFlag(self):
+        return self.exit_flag
     
     def setStartFlag(self, flag):
         self.start_flag = flag
         
     def setStopFlag(self, flag):
-        self.stop_flag = flag 
+        self.exit_flag = flag 
         
-    def start(self):       
-        s = threading.Thread(target = self.setStartFlag, args= (1,))
-        s.daemon = True
-        s.start()
-        #self.lectureTrack()
+    def resume(self):
+        resume.config(text = "Resume Tracking")
+        label.config(text = "Tracking Mode: Automatic Tracking")        
+        
+    def pause(self):
+        resume.config(text = "Pause Tracking")
+        label.config(text = "Tracking Mode: Manual Tracking")
+        
+    def resume_command(self):
+        resume.config(command = lambda: self.pause_resume())
+        self.pause()
+        
+    def pause_resume(self): # dynamically changes the text displayed on Pause/Resume button.
+        if self.start_flag == 1:
+            s = threading.Thread(target = self.setStartFlag, args= (0,))
+            v = threading.Thread(target = self.resume)        
+            s.daemon = True
+            s.start()
+            v.start()
+        else:
+            s = threading.Thread(target = self.setStartFlag, args= (1,))
+            v = threading.Thread(target = self.pause)        
+            s.daemon = True
+            s.start()
+            v.start()            
         
     def stop(self):        
         t = threading.Thread(target = self.setStartFlag, args= (0,))   
         t.daemon = True
-        t.start()
-    def printNumbers(self):
-        print self.exit_flag, self.start_flag        
+        t.start()        
         
     def begin(self):
         track = threading.Thread(target = self.lectureTrack)
-        numbers = threading.Thread(target = self.printNumbers)
-        numbers.daemon = True
+        button_command = threading.Thread(target = self.resume_command)
         track.daemon = True 
         track.start()
-        numbers.start()
+        button_command.start()
+        start.pack_forget()
         
     #detect face and return their rectangle
     def detectAndDisplay(self, frame, cascade):
@@ -176,10 +190,7 @@ class TrackingWindow:
 
         #while inside while to maintain loop and active program
         #while self.getStartFlag() == 1 or while self.getStartFlag() == 1
-        while self.exit_flag == 0:
-            print self.exit_flag, self.start_flag
-            
-                
+        while self.exit_flag == 0:              
             ret, frame = capture.read()
             j += 1
             
@@ -226,7 +237,6 @@ class TrackingWindow:
         #ip = raw_input("PORT: ") #23
         #ip = raw_input("ACCT: ") #admin
         #ip = raw_input("PW: ") #password
-#app = TrackingApp()
 window = TrackingWindow(1, 0)
 window.root.mainloop()
     
